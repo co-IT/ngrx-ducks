ac161db7e62bd5d715dc55f75d69f6d4cc94c7ad
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-21 07:34:27 +0100

    rxjs-operators: check if buffer is resetted

--- rxjs/operators/src/buffer-match.spec.ts
+++ rxjs/operators/src/buffer-match.spec.ts
@@ -1,40 +1,79 @@
-import { from } from 'rxjs';
+import { from, Subject } from 'rxjs';
 import { bufferMatch } from './buffer-match';
+import { MatchFn } from './types';
 
 function noop() {
   /* intentionally left blank */
 }
 
 describe('bufferMatch', () => {
+  let sequence: string;
+  let callback: Function;
+  let callBackOnMatch: MatchFn<number>;
+
+  beforeEach(() => {
+    sequence = '123';
+    callback = noop;
+    callBackOnMatch = b => (b.join('') === sequence ? callback : null);
+  });
+
   describe('When the sequence matches', () => {
     it('should emit the configured callback', done => {
-      // console.log(streamValues);
-
       const stream$ = from([1, 2, 3]);
-      const sequence = '123';
-      const callback = noop;
 
-      return stream$
-        .pipe(bufferMatch(b => (b.join('') === sequence ? callback : null)))
-        .subscribe(cb => {
-          expect(cb).toBe(callback);
-          done();
-        });
+      stream$.pipe(bufferMatch(callBackOnMatch)).subscribe(cb => {
+        expect(cb).toBe(callback);
+        done();
+      });
     });
   });
 
   describe('When the sequence does not match', () => {
     it('should not emit', () => {
       const stream$ = from([0, 0, 0]);
-      const sequence = '123';
       const observer = jest.fn();
-      const callback = noop;
 
-      stream$
-        .pipe(bufferMatch(b => (b.join('') === sequence ? callback : null)))
-        .subscribe(observer);
+      stream$.pipe(bufferMatch(callBackOnMatch)).subscribe(observer);
 
       expect(observer).not.toBeCalled();
     });
   });
+
+  describe('When the sequence does not match', () => {
+    it('should not emit', done => {
+      let isFirstCall = true;
+
+      const stream$ = from([0, 1, 2, 3]);
+      const resetNotifier$$ = new Subject<void>();
+      const callback = noop;
+
+      stream$
+        .pipe(
+          bufferMatch(b => {
+            isFirstCall = resetOnFirstCall(isFirstCall, resetNotifier$$);
+            return callBackOnMatch(b);
+          }, resetNotifier$$)
+        )
+        .subscribe(cb => {
+          expect(cb).toBe(callback);
+          done();
+        });
+    });
+  });
 });
+
+/**
+ *
+ * @param isFirstCall indicates if operator is executed the very first time
+ * @param resetNotifier$$ emits an event on the stream to reset the buffer inside buferMatch
+ */
+function resetOnFirstCall(
+  isFirstCall: boolean,
+  resetNotifier$$: Subject<void>
+) {
+  if (isFirstCall) {
+    resetNotifier$$.next();
+    isFirstCall = false;
+  }
+  return isFirstCall;
+}
