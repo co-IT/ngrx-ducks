8aa6c2b9c191e9a189385690dacb27cf36452c1c
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-25 18:11:02 +0100

    ngrx-ducks(createFrom): start with new factory creating a reducer function

---
+++ ngrx/ducks/src/lib/reducer/reducerFrom.ts
@@ -0,0 +1,32 @@
+import { NgrxDucksError } from '../core/errors';
+import { throwIf } from '../core/validation';
+
+class Plain {}
+
+describe('reducerFrom', () => {
+  describe('When the target has no initial value', () => {
+    it('should emit an error', () => {
+      const error = new NgrxDucksError(
+        `${createFrom.name}: ${
+          Plain.name
+        } does not define initialValue. Make sure to annotate ${
+          Plain.name
+        } with @InitialState.`
+      );
+      expect(() => createFrom(Plain)).toThrowError(error);
+    });
+  });
+});
+
+function createFrom<T extends new () => InstanceType<T>>(Token: T) {
+  const instance: { initialState?: unknown } = new Token();
+
+  throwIf(
+    instance.initialState === null || instance.initialState === undefined,
+    `${createFrom.name}: ${
+      Token.constructor.name
+    } does not define initialValue. Make sure to annotate ${
+      Token.constructor.name
+    } with @InitialState.`
+  );
+}
