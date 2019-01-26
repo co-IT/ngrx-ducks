b9886734159800afbc60ebd174e8567f147dc3c4
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-26 18:30:21 +0100

    ngrx-ducks: create reducer from annotated class

--- ngrx/ducks/src/lib/reducer/reducer-from.spec.ts
+++ ngrx/ducks/src/lib/reducer/reducer-from.spec.ts
@@ -1,3 +1,4 @@
+import { methodsFrom } from '../class';
 import { Action, InitialState } from '../decorators';
 import { NoInitialValueError, throwIf } from '../errors';
 import { nullOrUndefined } from '../validators';
@@ -13,30 +14,61 @@ describe('reducerFrom', () => {
   });
 
   describe('When a "Duck" provide an Action', () => {
+    const incrementActionType = 'increment count';
+
     @InitialState(0)
-    class Duck {
-      @Action('increment count')
+    class Counter {
+      @Action(incrementActionType)
       increment(state: number) {
-        return state++;
+        return ++state;
       }
     }
+
     it('should create the reducer function from it', () => {
-      const reducer = createFrom(Duck);
+      const reducer = createFrom(Counter);
       expect(reducer).toBeInstanceOf(Function);
+    });
+
+    it('should process an action', () => {
+      const reducer = createFrom(Counter);
+      const countState = reducer(undefined, { type: incrementActionType });
+
+      expect(countState).toBe(1);
     });
   });
 });
 
-function createFrom<T extends new () => InstanceType<T>>(Token: T) {
+export interface ActionThatMayHaveAPayload<TPayload = unknown> {
+  type: string;
+  payload?: TPayload;
+}
+
+export interface ReducerFunction<TState = unknown> {
+  (state: TState, action: ActionThatMayHaveAPayload): TState;
+}
+
+function createFrom<T extends new () => InstanceType<T>>(
+  Token: T
+): ReducerFunction {
   const instance: { initialState?: unknown } = new Token();
+
   const initialState = instance.initialState;
+  const actionReducerMap = methodsFrom(Token)
+    .map(name => instance[name].wiredAction)
+    .reduce(
+      (map, wiredAction) => ({
+        ...map,
+        [wiredAction.type]: wiredAction.caseReducer
+      }),
+      {}
+    );
 
   throwIf(
     nullOrUndefined(initialState),
     new NoInitialValueError(createFrom.name, Token.name)
   );
 
-  return function() {
-    /* */
+  return function(state = initialState, action: ActionThatMayHaveAPayload) {
+    return actionReducerMap[action.type](state, action.payload);
   };
 }
