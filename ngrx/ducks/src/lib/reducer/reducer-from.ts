b0088b9fdc5d4341f7daee5f455fcb981fdc8540
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-27 11:15:00 +0100

    ref(ngrx-ducks): extract createActionReducerMap to own file

--- ngrx/ducks/src/lib/reducer/reducer-from.ts
+++ ngrx/ducks/src/lib/reducer/reducer-from.ts
@@ -6,6 +6,7 @@ import {
   ReducerFunction
 } from '../typings';
 import { nullOrUndefined } from '../validators';
+import { createActionReducerMap } from './create-action-reducer-map';
 
 /**
  * Create reducer function from given class.
@@ -30,26 +31,4 @@ export function reducerFrom<T extends new () => InstanceType<T>>(
   ) {
     return actionReducerMap[action.type](state, action.payload);
   };
-}
-/**
- * Provide key value pair for action type and case reducer
- * It binds the class scope to the case reducer making it possible
- * to use the "this" scope inside each case reducer.
- *
- * @param methodNames method names of the class
- * @param instance of the class
- */
-function createActionReducerMap<T extends new () => InstanceType<T>>(
-  methodNames: string[],
-  instance: InitialStateAnnotated<unknown>
-) {
-  return methodNames
-    .map(name => (instance as any)[name].wiredAction)
-    .reduce(
-      (actionReducerMap, wiredAction) => ({
-        ...actionReducerMap,
-        [wiredAction.type]: wiredAction.caseReducer.bind(instance)
-      }),
-      {}
-    );
 }
