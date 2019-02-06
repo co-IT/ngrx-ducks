3ead767732856328f6156886b8328b2de9351ae0
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-02-06 15:29:27 +0100

    ref(ngrx-ducks): extract createDuckService to own file

--- ngrx/ducks/src/public_api.ts
+++ ngrx/ducks/src/public_api.ts
@@ -1,6 +1,6 @@
-export * from './lib/core/types/ducks/ducks';
 export * from './lib/core/create-reducer-from/create-reducer-from';
 export * from './lib/core/ducks/create-ducks';
 export * from './lib/core/ducks/effect';
+export * from './lib/core/types/ducks/ducks';
 export * from './lib/core/wired-actions/create-wired-action';
 export * from './lib/core/wired-actions/wire-up-actions';
