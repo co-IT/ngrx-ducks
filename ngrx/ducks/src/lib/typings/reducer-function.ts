fa0d9d2c64de9dea92bedccd6fbd9c5f2f9731de
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-02-07 12:36:57 +0100

    fix(ngrx-ducks): relax typing for ReducerFunction

--- ngrx/ducks/src/lib/typings/reducer-function.ts
+++ ngrx/ducks/src/lib/typings/reducer-function.ts
@@ -1,4 +1,4 @@
 import { ActionThatMayHaveAPayload } from './action-that-may-have-a-payload';
-export interface ReducerFunction<TState = unknown> {
+export interface ReducerFunction<TState = any> {
   (state: TState, action: ActionThatMayHaveAPayload): TState;
 }
