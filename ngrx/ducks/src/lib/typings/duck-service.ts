dd54efef1cf3cd6de87278ad0f9677a227740554
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-02-10 11:04:21 +0100

    fix(ngrx-ducks): update reference to right type

--- ngrx/ducks/src/lib/typings/duck-service.ts
+++ ngrx/ducks/src/lib/typings/duck-service.ts
@@ -1,5 +1,5 @@
-import { PickSelector } from '../__deprecated__/types';
 import { DuckActionDispatcher } from './duck-action-dispatcher';
+import { PickSelector } from './pick-selector';
 
 export type Duck<T> = { [K in keyof T]: DuckActionDispatcher<T[K]> } &
   PickSelector;
