d1fe38fb8f288fba8c88551e35d7a85963fd4f2d
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-03 17:36:09 +0100

    fix(ngrx-ducks-schematics): move schematic templates to dist/

--- ngrx/ducks-schematics/build/index.ts
+++ ngrx/ducks-schematics/build/index.ts
@@ -8,7 +8,7 @@ clean('./dist')
       ['./package.json', './dist'],
       ['./src/collection.json', './dist'],
       ['./src/**/schema.json', './dist'],
-      ['./src/**/templates/**/*.ts', './dist']
+      ['./src/**/files/**/*.ts.template', './dist']
     ])
   )
   .catch(err => {
