12e064d207f6db8d9f641d24209779261c33d996
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-06-03 10:36:23 +0200

    docs: explain why __initialState__ needs to be excluded from method name list

--- packages/ducks/src/class/method-from.ts
+++ packages/ducks/src/class/method-from.ts
@@ -1,6 +1,10 @@
 /**
  * Yields all methods from a certain class token.
  *
+ * The property __initialState_ needs to be excluded since it is only used
+ * to setup the initial state for the reducer function.
+ * We do not want to patch __initialState__ to become a self-dispatching-action.
+ *
  * @param classToken Token containing a prototype chain
  */
 export function methodsFrom<T extends new () => InstanceType<T>>(
