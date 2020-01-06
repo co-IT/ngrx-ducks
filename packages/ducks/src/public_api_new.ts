b8f4a5c58015dff1bd8890c3222a22846389bc2a
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-01-06 09:48:37 +0100

    feat(create-duck): make new API public 🚀

---
+++ packages/ducks/src/public_api_new.ts
@@ -0,0 +1,6 @@
+export * from './bind-selectors';
+export * from './create-duck';
+export * from './get-actions';
+export * from './get-reducer';
+export * from './store-facade';
+export * from './use-pick';
