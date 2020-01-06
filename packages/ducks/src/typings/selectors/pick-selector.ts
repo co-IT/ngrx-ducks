d94651cb250d536b0eb7282f2198ab45fa3574a4
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-01-06 09:27:44 +0100

    feat(usePick): provide factory for usePick

--- packages/ducks/src/typings/selectors/pick-selector.ts
+++ packages/ducks/src/typings/selectors/pick-selector.ts
@@ -2,12 +2,16 @@ import { MemoizedSelector, MemoizedSelectorWithProps } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 export type PickSelector = {
-  pick<TState, TProps, TResult>(
+  pick: PickFunction;
+};
+
+export interface PickFunction {
+  <TState, TProps, TResult>(
     selector: MemoizedSelectorWithProps<TState, TProps, TResult>,
     props: TProps
   ): Observable<TResult>;
 
-  pick<TState, TResult>(
-    selector: MemoizedSelector<TState, TResult>
-  ): Observable<TResult>;
-};
+  <TState, TResult>(selector: MemoizedSelector<TState, TResult>): Observable<
+    TResult
+  >;
+}
---
+++ packages/ducks/src/use-pick/use-pick.ts
@@ -0,0 +1,13 @@
+import { DucksIdentifier } from '../create-duck/create-duck';
+import { NgRxDucksNotConnectedError } from '../create-duck/create-duck-not-connected.error';
+import { PickFunction } from '../typings';
+
+export function usePick(): PickFunction {
+  const pickFacade = () => {
+    throw new NgRxDucksNotConnectedError();
+  };
+
+  pickFacade.__ngrx_ducks__id = DucksIdentifier.DuckPickFunction;
+
+  return pickFacade;
+}
