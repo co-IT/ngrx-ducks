47b022a547a87bfbf2b9e7a1a88af86e74b82789
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-01-18 16:55:07 +0100

    test(@Action): fix typings in test

--- packages/ducks/src/decorators/action.decorator.spec.ts
+++ packages/ducks/src/decorators/action.decorator.spec.ts
@@ -3,15 +3,15 @@ import { Action } from './action.decorator';
 
 describe('@Action', () => {
   describe('When no action type is given', () => {
-    it.each([[null], [undefined], [''], [[]]] as any)(
+    it.each([[null], [undefined], [''], [[]]] as any[])(
       'should raise an error',
       invalidType => {
         class Plain {}
         const error = new MissingActionTypeError(Plain.name);
 
-        expect(() =>
-          Action(invalidType as any)(Plain, '', null as any)
-        ).toThrowError(error);
+        expect(() => Action(invalidType)(Plain, '', null as any)).toThrowError(
+          error
+        );
       }
     );
   });
