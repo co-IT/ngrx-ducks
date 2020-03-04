170df9e67c5f10c415adffe51f354dd312f75acd
Thomas Skalnik <thomas.skalnik@siemens.com>
2020-03-04 15:52:00 +0100

    fix(@ducksify): set initial state

--- packages/core/src/decorator-api/decorators/ducksify.decorator.ts
+++ packages/core/src/decorator-api/decorators/ducksify.decorator.ts
@@ -1,21 +1,19 @@
 import { ɵɵdefineInjectable, ɵɵinject } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { ducksify } from '../ducks';
 import { notConstructableError } from '../../store-facade/store-facade';
+import { ducksify } from '../ducks';
 import { AnnotationTarget, DucksifyConfig } from '../typings';
 import { InitialState } from './initial-state.decorator';
 
 export function Ducksify<T>(config: DucksifyConfig<T>) {
   return function(constructor: AnnotationTarget) {
+    InitialState(config.initialState)(constructor);
     constructor.ɵfac = notConstructableError;
     constructor.ɵprov = ɵɵdefineInjectable({
       token: constructor,
       providedIn: 'root',
       factory() {
-        return ducksify(
-          InitialState(config.initialState)(constructor),
-          ɵɵinject(Store)
-        );
+        return ducksify(constructor, ɵɵinject(Store));
       }
     });
 
