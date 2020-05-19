2a509e98b52980ece8cb68d09621c7aa7405e729
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-05-19 20:43:44 +0200

    fix(core): ignore ngrx ducks internal members getting reducer functions

--- packages/core/src/get-reducer/get-reducer.ts
+++ packages/core/src/get-reducer/get-reducer.ts
@@ -22,14 +22,25 @@ function resolveReducers(
   instance: any,
   collectedReducers: { [key: string]: Function } = {}
 ): { [key: string]: Function } {
-  return Object.keys(instance).reduce(
-    (reducers, property) =>
-      !instance[property].reducer
-        ? resolveReducers(instance[property], reducers)
-        : {
-            ...reducers,
-            [instance[property].type]: instance[property].reducer
-          },
-    collectedReducers
+  return Object.keys(instance).reduce((reducers, property) => {
+    if (instance[property].reducer) {
+      return {
+        ...reducers,
+        [instance[property].type]: instance[property].reducer
+      };
+    } else if (
+      isNoNgRxDuckPatchInternal(instance[property]) &&
+      Object.keys(instance[property]).length > 0
+    ) {
+      return resolveReducers(instance[property], reducers);
+    } else {
+      return reducers;
+    }
+  }, collectedReducers);
+}
+
+function isNoNgRxDuckPatchInternal(object: any) {
+  return Object.keys(object).every(
+    property => !property.match(/__ngrx_ducks__/)
   );
 }
