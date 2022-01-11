c00b243c180d6eae8597c0f318623c512561e4c1
Thomas Skalnik <thomas.skalnik@gmail.com>
2022-01-11 17:05:59 +0100

    fix(has-immutable-duck): consider nested ducks

--- libs/core/src/lib/store-chunk/reducer-registration/has-immutable-duck.ts
+++ libs/core/src/lib/store-chunk/reducer-registration/has-immutable-duck.ts
@@ -1,7 +1,10 @@
 import { isImmutableDuck } from '../connect';
 
 export function hasImmutableDuck(instance: any): boolean {
-  return Object.keys(instance).some(property =>
-    isImmutableDuck(instance, property)
+  return Object.keys(instance).some(
+    (property) =>
+      isImmutableDuck(instance, property) ||
+      (Object.keys(instance[property]).length > 0 &&
+        hasImmutableDuck(instance[property]))
   );
 }
