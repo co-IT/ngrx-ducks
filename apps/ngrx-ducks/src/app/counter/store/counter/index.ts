b848123955ed4603ed1862c62c38555c7917b6f4
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-11-20 15:31:30 +0100

    fix: apply work around for useActions

--- apps/ngrx-ducks/src/app/counter/store/counter/index.ts
+++ apps/ngrx-ducks/src/app/counter/store/counter/index.ts
@@ -1,5 +1,5 @@
 export * from './counter-immutable.selectors';
 export * from './counter.effects';
-export * from './counter.facade';
 export * from './counter.state';
 export * from './counter.store';
+
